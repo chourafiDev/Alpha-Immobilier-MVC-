0ed09ea2f2dad9e448846d6fbc7059faad7cd826
chourafiDev <abdelmonaimechourafi@gmail.com>
2021-04-08 16:16:38 +0100

    first commit

---
+++ Alpha Immobilier Project/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alpha_Immobilier_Project.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
